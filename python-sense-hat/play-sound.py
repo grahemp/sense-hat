bc33a30b71df86c36487b11897b039b9cb52f935
grahemp <pmeharg@swbell.net>
2017-09-06 12:08:27 -0500

    keeping things up to date

--- python-sense-hat/play-sound.py
+++ python-sense-hat/play-sound.py
@@ -1,8 +1,9 @@
 import subprocess
+# sound-file='/home/pi/sound/328827__magedu__bicycle-bell-05.wav'
 # p = subprocess.Popen(['omxplayer', '/home/pi/sound/105345__sandyrb__sonar-ping-pong-eb.wav'], stdout=subprocess.PIPE.communicate()[0]
 
 
-p = subprocess.Popen (['omxplayer', '/home/pi/sound/105345__sandyrb__sonar-ping-pong-eb.wav'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+p = subprocess.Popen (['omxplayer', '/home/pi/sound/328827__magedu__bicycle-bell-05.wav'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 out, err = p.communicate()
 print out
